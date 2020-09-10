ff1fd5bdfd8b21d6fd8a05215ff85e4904f31fb2
Victor Park <victor.shpark@gmail.com>
2020-09-10 04:58:46 -0300

    logic of exercise implemented in service objects!

---
+++ app/services/withdrawal_service.rb
@@ -0,0 +1,24 @@
+class WithdrawalService
+attr_reader :amount
+
+  def initialize(amount)
+    @amount = amount.to_i
+  end
+
+  private
+
+  def notes_amount
+    {
+      :G => (amount / 100),
+      :O=> (amount % 100 / 50),
+      :M => (amount % 100 % 50 / 20),
+      :A => (amount % 100 % 50 % 20 / 10),
+     }.reject {|_, v| v == 0}
+  end
+
+  # FUN FACT! G-O-M-A represents the abbreviated names of the animals in Brazilian bills
+    # Garoupa => 100
+    # Onca-pintada => 50
+    # Mico-leao-dourado => 20
+    # Arara-vermelha => 10
+end
